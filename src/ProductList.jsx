bbf5b0aadb566893838e42fab8f8576f1a559232
ralfschmid3 <ralf.schmid3@gmx.de>
2025-08-27 21:45:49 +0200

    BugFixes and Finished Step 3.

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
+import { useDispatch } from 'react-redux';
 import CartItem from './CartItem';
-import addItem from './CartSlice';
+import { addItem } from './CartSlice';
 
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
-    
+    const dispatch = useDispatch();
     
     const plantsArray = [
         {
@@ -288,13 +289,13 @@ function ProductList({ onHomeClick }) {
           </div>
           {!showCart ? (
               <div className="product-grid">
-                {plantsArray.map((category, index) => {
+                {plantsArray.map((category, index) => 
                     <div key={index}>
                       <h1>
                         <div>{category.category}</div>
                       </h1>
                       <div className="product-list">
-                        {category.plants.map((plant,plantIndex) => {
+                        {category.plants.map((plant,plantIndex) => 
                             <div className="product-card" key={plantIndex}>
                               <img
                                 className="product-image"
@@ -311,10 +312,10 @@ function ProductList({ onHomeClick }) {
                                 Add to Cart
                               </button>
                             </div>
-                        })}
+                        )}
                       </div>
                     </div>
-                })}
+                )}
               </div>
           ) : (
               <CartItem onContinueShopping={handleContinueShopping} />

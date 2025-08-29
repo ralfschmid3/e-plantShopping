5c8d85ce428568146d00511e7652fa690ec36dc1
ralfschmid3 <ralf.schmid3@gmx.de>
2025-08-29 16:21:29 +0200

    Finished Step 3.

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -4,6 +4,7 @@ export const CartSlice = createSlice({
     name: 'cart',
     initialState: {
         items: [], // Initialize items as an empty array
+        totalQuantity: 0,
     },
     reducers: {
         addItem: (state, action) => {
@@ -14,14 +15,18 @@ export const CartSlice = createSlice({
             } else {
                 state.items.push({ name, image, cost, quantity: 1});
             }
+            state.totalQuantity++;
         },
         removeItem: (state, action) => {
+            const itemToRemove = state.items.find(item => item.name === action.payload);
             state.items = state.items.filter(item => item.name !== action.payload);
+            state.totalQuantity -= itemToRemove.quantity;
         },
         updateQuantity: (state, action) => {
             const { name, quantity } = action.payload;
             const itemToUpdate = state.items.find(item => item.name === name);
             if (itemToUpdate) {
+                state.totalQuantity = state.totalQuantity - itemToUpdate.quantity + quantity;
                 itemToUpdate.quantity = quantity;
             }
         },
